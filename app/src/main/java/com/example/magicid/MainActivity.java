3165ffee4406b506f8a341198f94e3f44acd5f0f
deep <deepanshusehgal.112@gmail.com>
2020-07-09 18:10:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/magicid/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.magicid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText etID;
+    Button btSummit;
+    TextView tvResults;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etID=findViewById(R.id.etID);
+        btSummit=findViewById(R.id.btSummit);
+        tvResults=findViewById(R.id.tvResults);
+
+        tvResults.setVisibility(View.GONE);
+
+        btSummit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String idNumber = etID.getText().toString().trim();
+                String dob=idNumber.substring(0, 6);
+                int gender=Integer.parseInt(Character.toString(idNumber.charAt(6)));
+                String sGender;
+                if (gender < 5)
+                    sGender= getString(R.string.female);
+                else
+                    sGender= getString(R.string.male);
+                int nationality=Integer.parseInt(Character.toString(idNumber.charAt(10)));
+                String sNationality;
+                if (nationality == 0)
+                    sNationality=getString(R.string.sacitizen);
+                else
+                    sNationality=getString(R.string.permanentcitizen);
+                String text=getString(R.string.dob) + dob + "\n" + getString(R.string.gender) + sGender +
+                        "\n" + getString(R.string.nationality) + sNationality;
+                tvResults.setText(text);
+
+                tvResults.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
